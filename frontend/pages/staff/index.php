dfb39b10721ab4801c489eeccaedb5cb5bc6cfe2
Duy An Ngo Nguyen <40843946+ZoroZero@users.noreply.github.com>
2020-12-07 21:10:18 +0700

    Update index.php

--- frontend/pages/staff/index.php
+++ frontend/pages/staff/index.php
@@ -4,7 +4,7 @@ include_once("../backend/environments/Constants.php");
 if (!isset($_SESSION[USER_ID]) || !isset($_SESSION[ROLE_ID])) {
     header("Location: ./login");
 } else {
-    if($_SESSION[ROLE_ID] == 2){
+    if($_SESSION[ROLE_ID] == 2 || $_SESSION[ROLE_ID] == 3){
         include_once('../frontend/components/header/header.php');
         include('../frontend/components/staff/index.php');
         echo "<script type='module' src='../frontend/pages/staff/index.js'></script>";
