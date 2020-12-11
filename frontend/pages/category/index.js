7b4e4258d3bbd98a540ef5f816f4a707ddf12f6c
Duy An Ngo Nguyen <40843946+ZoroZero@users.noreply.github.com>
2020-12-11 22:07:03 +0700

    Fix routing

--- frontend/pages/category/index.js
+++ frontend/pages/category/index.js
@@ -1,39 +1,82 @@
+import {formatPrice, getProductCategory } from '../../index.js';
+
 $(document).ready(function() {
-
+  
+  fetchProduct();
   //isotope filter
-  var $grid = $(".grid").isotope({
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows',
-    getSortData: {
-        productname: '.product-name',
-        productprice: '.product-price'
-    }
-  })
+});
 
-  $grid.isotope({filter: '*'})
 
-  //filter items on button press
-  $(".button-group").on("click", "button", function(){
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({filter: filterValue});
-  })
+function fetchProduct(){
+  $.get(`../backend/product/GetAllProduct.php`,
+      function(response) {
+        if(response){
+          if(!JSON.parse(response)['error']){
+            var tableProductList = JSON.parse(response)['data'];
+            console.log("Cart products: ",tableProductList);
+            var list_product = tableProductList.map(function(product){
+              return `<div class="grid-item Mac border">
+                        <div class="item py-2 px-2" style="width: 200px;">
+                            <div class="product font-rale">
+                                <a href="#">
+                                    <img src="./assets/imgs/homepage/mac/product10.png" alt="product10" class="img-fluid">
+                                </a>
+                                <div class="text-center">
+                                    <h6 class="product-name">
+                                        PVP (Mac)
+                                    </h6>
+                                    <div class="rating text-warning font-size-12">
+                                        <span>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star-half-alt"></i>
+                                        </span>
+                                    </div>
+                                    <div class="price py-2">
+                                        <span class="product-price">14,000,000đ</span>
+                                    </div>
+                                    <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`});
+            $('#products-grid').html(list_product.join(' '));
+            console.log(list_product);
+          }
+          else{
+              console.log("Error ", response['message']);
+          }
+        }
+        var $grid = $(".grid").isotope({
+          itemSelector: '.grid-item',
+          layoutMode: 'fitRows',
+          getSortData: {
+              productname: '.product-name',
+              productprice: '.product-price'
+          }
+        })
+      
+        $grid.isotope({filter: '*'})
+      
+        //filter items on button press
+        $(".button-group").on("click", "button", function(){
+          var filterValue = $(this).attr('data-filter');
+          $grid.isotope({filter: filterValue});
+        })
+      
+        $(".button-group-sort").on("click", "button", function(){
+          var filterValue = $(this).attr('data-filter');
+          console.log(filterValue)
 
-  $(".button-group-sort").on("click", "button", function(){
-    var filterValue = $(this).attr('data-filter');
-    console.log(filterValue)
-    /* Get the element name to sort */
-    // var sortValue = $(this).attr('data-sort-value');
-
-    /* Get the sorting direction: asc||desc */
-    var direction = $(this).attr('direction');
-
-    /* convert it to a boolean */
-    var isAscending = (direction == 'asc');
-    // var newDirection = (isAscending) ? 'desc' : 'asc';
-    /* pass it to isotope */
-    $grid.isotope({ sortBy: filterValue, sortAscending: isAscending });
-
-    // $(this).attr('data-sort-direction', newDirection);
-
-  })
-})
+          var direction = $(this).attr('direction');
+      
+          /* convert it to a boolean */
+          var isAscending = (direction == 'asc');
+          // var newDirection = (isAscending) ? 'desc' : 'asc';
+          /* pass it to isotope */
+          $grid.isotope({ sortBy: filterValue, sortAscending: isAscending });
+        })
+    });
+}
--- frontend/pages/homepage/index.php
+++ frontend/pages/homepage/index.php
@@ -1,16 +1,14 @@
 <?php
 
-include_once("../backend/environments/Constants.php");
-if (!isset($_SESSION[USER_ID])) {
-    header("Location: ./login");
-} else {
-    require_once('../frontend/components/header/header.php');
-    include('../frontend/components/homepage/__banner_erea.php');
-    include('../frontend/components/homepage/__top_sale.php');
-    include('../frontend/components/homepage/__special_price.php');
-    include('../frontend/components/homepage/__banner_ads.php');
-    include('../frontend/components/homepage/__new_pc.php');
-    include('../frontend/components/homepage/__blogs.php');
-    echo "<script type='module' src='../frontend/pages/homepage/index.js'></script>";
-    require_once('../frontend/components/footer/footer.php');
-}
+// include_once("../backend/environments/Constants.php");
+
+require_once('../frontend/components/header/header.php');
+include('../frontend/components/homepage/__banner_erea.php');
+include('../frontend/components/homepage/__top_sale.php');
+include('../frontend/components/homepage/__special_price.php');
+include('../frontend/components/homepage/__banner_ads.php');
+include('../frontend/components/homepage/__new_pc.php');
+include('../frontend/components/homepage/__blogs.php');
+echo "<script type='module' src='../frontend/pages/homepage/index.js'></script>";
+require_once('../frontend/components/footer/footer.php');
+
